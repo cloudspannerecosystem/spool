1652499aba7a62b7aeb04c48722cb4db963222b9
shuheiktgw <s-kitagawa@mercari.com>
2021-12-29 10:42:49 +0900

    Update dependencies

--- tools/tools.go
+++ tools/tools.go
@@ -1,8 +1,8 @@
 package tools
 
 import (
+	_ "github.com/cloudspannerecosystem/wrench"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/mercari/wrench"
 	_ "github.com/rakyll/statik"
 	_ "go.mercari.io/yo"
 )
